8d3cce286b19cd8c12adf47bf2bc83ed576ccabe
sudarshan604 <sudarshandn1999@gmail.com>
2022-03-11 00:03:21 +0545

    m dice

---
+++ javascript project/dice game/index.js
@@ -0,0 +1,29 @@
+let playerOneScore=0
+let playerTwoScore=0
+let playerTrun=true
+
+let rollBtn=document.getElementById('roll')
+let restBtn=document.getElementById('reset')
+
+
+let player1ScorderBoard=document.getElementById('playeronescore')
+let player2ScorderBoard=document.getElementById('playertwoscore')
+
+let player1Board=document.getElementById('box-1')
+let player2Board= document.getElementById('box-2')
+
+
+rollBtn.addEventListener('click',function(){
+
+
+})
+
+
+
+
+
+
+
+
+
+
