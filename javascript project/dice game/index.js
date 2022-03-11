4cfda99a66fbfac0c0b319713496509149a7e146
sudarshan604 <sudarshandn1999@gmail.com>
2022-03-12 00:21:08 +0545

    --

--- javascript project/dice game/index.js
+++ javascript project/dice game/index.js
@@ -13,10 +13,29 @@ let player1Board=document.getElementById('box-1')
 let player2Board= document.getElementById('box-2')
 
 
+
+  
+
 rollBtn.addEventListener('click',function(){
+     let randomNumber=Math.floor(Math.random()*6)+1    
+    if(playerTrun){
+          player1Board.textContent=randomNumber
+         playerOneScore+=randomNumber
+         player1ScorderBoard.textContent= "Score:"+playerOneScore
+         player1Board.style.
+         playerTrun= false
+       
+        }
+    else{
+      player2Board.textContent=randomNumber
+      playerOneScore+=randomNumber
+      player2ScorderBoard.textContent= "Score:"+playerOneScore
+      playerTrun=!playerTrun
+    }
 
 
-})
+
+    })
 
 
 
